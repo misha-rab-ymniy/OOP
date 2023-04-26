701aa756b50184976e575ed6872ec8d797b98250
misha-rab-ymniy <mihaildubinka@gmail.com>
2023-04-26 09:07:30 +0300

    add constants

---
+++ src/constants.py
@@ -0,0 +1,10 @@
+LEAGUES_ID = {
+    'EPL': 94,
+    'Champions League': 1040,
+    'SeriaA': 199,
+    'LaLiga': 207,
+    'Bundesliga': 123,
+    'Ligue1': 99,
+}
+
+TWO_YEAR_FORMAT = [94, 1040, 199, 207, 123, 99]
